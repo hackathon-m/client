bd77f58cc6403abd138efdacb97531c5b7064823
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2024-06-01 17:42:25 +0900

    kakao login

--- src/screens/LandingScreen.tsx
+++ src/screens/LandingScreen.tsx
@@ -1,12 +1,52 @@
+import { login, KakaoOAuthToken } from '@react-native-seoul/kakao-login';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const LandingScreen = () => {
+  const kakaoLogin = async () => {
+    try {
+      const response: KakaoOAuthToken = await login();
+
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <View>
-      <Text>랜딩 페이지</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.logo}>
+        <Text>가운데</Text>
+      </View>
+
+      <Pressable style={styles.button} onPress={kakaoLogin}>
+        <Text style={styles.buttonText}>카카오 로그인</Text>
+      </Pressable>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  logo: {
+    flex: 1,
+  },
+  button: {
+    backgroundColor: '#FEE500',
+    paddingVertical: 14,
+    paddingHorizontal: 20,
+    marginHorizontal: 20,
+    borderRadius: 12,
+  },
+  buttonText: {
+    color: '#3C1E1E',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
 
 export default LandingScreen;
