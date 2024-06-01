9d854498cbf88f75d2f2fd69338ef46c759b4eb3
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2024-06-01 14:32:00 +0900

    initial settings

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,32 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    // alias 설정
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+        alias: {
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@pages': './src/pages',
+          '@type': './src/type', // types는 불가능해서 type으로 대체
+          '@utils': './src/utils',
+          '@assets': './src/assets',
+          '@server': './src/server',
+          '@axios': './src/axios',
+        },
+      },
+    ],
+  ],
 };
