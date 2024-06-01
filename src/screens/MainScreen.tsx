7633738929c4e9e8520a79342a905dcd2fdeeb55
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2024-06-01 21:32:21 +0900

    tab navi

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -8,6 +8,12 @@ import SuggestionScreen from './SuggestionScreen';
 
 import { TabNavigatorParamList } from '@type/params/loginStack';
 
+import HomeNavigation from '@assets/images/HomeNavigation.svg';
+import BattleNavigation from '@assets/images/BattleNavigation.svg';
+import SuggestionNavigation from '@assets/images/SuggestionNavigation.svg';
+import MyPageNavigation from '@assets/images/MyPageNavigation.svg';
+import { Text, View } from 'react-native';
+
 const Tab = createBottomTabNavigator<TabNavigatorParamList>();
 
 const MainScreen = () => {
@@ -15,12 +21,85 @@ const MainScreen = () => {
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
+        tabBarStyle: {
+          backgroundColor: '#262626',
+          padding: 24,
+          height: 90,
+        },
       }}
     >
-      <Tab.Screen name="HomeScreen" component={HomeScreen} />
-      <Tab.Screen name="SuggestionScreen" component={SuggestionScreen} />
-      <Tab.Screen name="MyCouponScreen" component={MyCouponScreen} />
-      <Tab.Screen name="MyPageScreen" component={MyPageScreen} />
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={({ route }) => ({
+          // title 없애고 custom 하기 위한 옵션
+          tabBarLabel: () => {
+            return null;
+          },
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <HomeNavigation />
+                <Text style={{ color: 'white', marginTop: 8 }}>홈</Text>
+              </View>
+            );
+          },
+        })}
+      />
+      <Tab.Screen
+        name="SuggestionScreen"
+        component={SuggestionScreen}
+        options={({ route }) => ({
+          // title 없애고 custom 하기 위한 옵션
+          tabBarLabel: () => {
+            return null;
+          },
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <BattleNavigation />
+                <Text style={{ color: 'white', marginTop: 8 }}>배틀</Text>
+              </View>
+            );
+          },
+        })}
+      />
+      <Tab.Screen
+        name="MyCouponScreen"
+        component={MyCouponScreen}
+        options={({ route }) => ({
+          // title 없애고 custom 하기 위한 옵션
+          tabBarLabel: () => {
+            return null;
+          },
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <SuggestionNavigation />
+                <Text style={{ color: 'white', marginTop: 8 }}>제안</Text>
+              </View>
+            );
+          },
+        })}
+      />
+      <Tab.Screen
+        name="MyPageScreen"
+        component={MyPageScreen}
+        options={({ route }) => ({
+          // title 없애고 custom 하기 위한 옵션
+          tabBarLabel: () => {
+            return null;
+          },
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <MyPageNavigation />
+                <Text style={{ color: 'white', marginTop: 8 }}>마이</Text>
+              </View>
+            );
+          },
+        })}
+      />
     </Tab.Navigator>
   );
 };
